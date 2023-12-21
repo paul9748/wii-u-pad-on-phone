3706403f6533e78e254d3ad10a9b8c993e1ad062
paul9748 <paul9748@hotmail.co.kr>
2023-12-21 18:56:36 +0900

    init

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "\\views");
+app.get("/", (req, res) => res.render("home"));
+app.listen(3000);
