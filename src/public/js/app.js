0753f0065567946a9c0b64e3c06725a67b518c22
paul9748 <paul9748@hotmail.co.kr>
2023-12-28 16:44:32 +0900

    ws test

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,28 +3,44 @@ var socket = new WebSocket(`ws://${window.location.host}`);
 
 
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 
-socket.addEventListener('open',()=>{
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
+
+socket.addEventListener('open', () => {
     console.log('connecting to server');
 });
 
-socket.addEventListener('message',(message)=>{
+socket.addEventListener('message', (message) => {
     // console.log('received message:',message.data);
     const li = document.createElement('li');
     li.innerText = message.data;
     messageList.append(li);
 });
 
-socket.addEventListener('close',()=>{console.log('disconnecting from server');});
+socket.addEventListener('close', () => { console.log('disconnecting from server'); });
 
-function handlesubmit(event){
+function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector('input');
     console.log(input.value);
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement('li');
+    li.innerText = `You:${input.value}`;
+    messageList.append(li);
     input.value = '';
 };
+function handleNickSubmit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
 
-messageForm.addEventListener('submit',handlesubmit);
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
 
--- src/server.js
+++ src/server.js
@@ -7,25 +7,35 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "\\views");
-app.use("/public", express.static(__dirname +"/public"));
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log("ws://localhost:3000");
 
-const server = http.createServer(app) ;
+const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 
 const sockets = []
 
-wss.on("connection",(socket) =>{
+wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anonymous";
     console.log("connecting to browser");
-    socket.on("close", () =>{console.log("disconnected from browser");});
-    socket.on("message", (message)=>{
-        sockets.forEach(aSocket =>{
-        aSocket.send(message.toString());
-    })
+    socket.on("close", () => { console.log("disconnected from browser"); });
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg.toString());
+        console.log(message);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach(aSocket => {
+                    aSocket.send(`${socket.nickname}: ${message.payload}`);
+                });
+            case "nickname":
+                console.log(message.payload)
+                socket["nickname"] = message.payload;
+        };
+
     });
 
 });
