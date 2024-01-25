694f7941506f0767e42ab67d571ba0f6d3a5b821
paul9748 <paul9748@hotmail.co.kr>
2024-01-26 00:13:11 +0900

    save

--- src/public/js/cli.js
+++ src/public/js/cli.js
@@ -1,192 +1,190 @@
 const socket = io();
 let scale = 1;
-let exTime = + new Date();;
+let exTime = +new Date();
+let exGamepadState;
 
 const scaleSelect = document.getElementById("scale");
-const fullBtn = document.getElementById("full");
-const myFace = document.getElementById("myFace");
+const fullBtn = document.getElementById("Fullscreen");
+const screen = document.getElementById("screen");
+const video = document.getElementById("video");
 const roomName = "upad";
 
 scaleSelect.addEventListener("change", handleScaleChange);
 
 function handleScaleChange() {
-    scale = scaleSelect.value;
-    console.log(scale);
+  scale = scaleSelect.value;
+  console.log(scale);
 }
 window.ondevicemotion = function (motion) {
-    //x(red) z(blue) y(green) 
-    var gyroV = {
-        x: scale * motion.rotationRate.alpha,
-        z: scale * motion.rotationRate.beta,
-        y: -scale * motion.rotationRate.gamma,
-    }
-    var gyroH = {
-        z: scale * motion.rotationRate.alpha,
-        x: -scale * motion.rotationRate.beta,
-        y: -scale * motion.rotationRate.gamma,
-    }
+  //x(red) z(blue) y(green)
+  var gyroV = {
+    x: scale * motion.rotationRate.alpha,
+    z: scale * motion.rotationRate.beta,
+    y: -scale * motion.rotationRate.gamma,
+  };
+  var gyroH = {
+    z: scale * motion.rotationRate.alpha,
+    x: -scale * motion.rotationRate.beta,
+    y: -scale * motion.rotationRate.gamma,
+  };
 
-    document.getElementById("x").textContent = 'x:' + gyroV.x
-    document.getElementById("y").textContent = 'y:' + gyroV.y
-    document.getElementById("z").textContent = 'z:' + gyroV.z
-    var data = {
-        ts: new Date().getTime(),
-        gyro: screen == 'v' ? gyroV : gyroH,
-        acceleration: {
-            x: 0,
-            y: 0,
-            z: 0
-        }
-    }
-    // console.log(gyroV);
-    set
+  document.getElementById("x").textContent = "x:" + gyroV.x;
+  document.getElementById("y").textContent = "y:" + gyroV.y;
+  document.getElementById("z").textContent = "z:" + gyroV.z;
+  var data = {
+    ts: new Date().getTime(),
+    gyro: screen == "v" ? gyroV : gyroH,
+    acceleration: {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+  };
+  // console.log(gyroV);
 };
 
 // Gamepad part
 // 게임패드 입력 상태를 저장할 객체
-const gamepadState = {};
+let gamepadState = {};
 
 // 게임패드 연결 이벤트 리스너
 window.addEventListener("gamepadconnected", (event) => {
-    const gamepad = event.gamepad;
-    console.log(`게임패드가 연결되었습니다: ${gamepad.id}`);
+  const gamepad = event.gamepad;
+  console.log(`게임패드가 연결되었습니다: ${gamepad.id}`);
 
-    // 새로 연결된 게임패드의 상태를 초기화
-    gamepadState[gamepad.index] = {};
+  // 새로 연결된 게임패드의 상태를 초기화
+  gamepadState[gamepad.index] = {};
 });
 
 // 게임패드 연결 해제 이벤트 리스너
 window.addEventListener("gamepaddisconnected", (event) => {
-    const gamepad = event.gamepad;
-    console.log(`게임패드가 연결이 해제되었습니다: ${gamepad.id}`);
-    // 연결 해제된 게임패드의 상태 삭제
-    delete gamepadState[gamepad.index];
+  const gamepad = event.gamepad;
+  console.log(`게임패드가 연결이 해제되었습니다: ${gamepad.id}`);
+  // 연결 해제된 게임패드의 상태 삭제
+  delete gamepadState[gamepad.index];
 });
 
 // 주기적으로 게임패드 상태 감지 및 출력
 const pollingInterval = 0; // 밀리초 단위로 감지 주기 설정
 
 function updateGamepadState() {
-    const gamepads = navigator.getGamepads();
-    const exGamepadState = gamepadState;
-    for (const gamepad of gamepads) {
-        if (gamepad) {
-            // 게임패드의 모든 버튼 및 축을 순회하면서 상태 업데이트
-            gamepadState[gamepad.index] = {};
+  const gamepads = navigator.getGamepads();
+  exGamepadState = JSON.parse(JSON.stringify(gamepadState)); // 깊은 복사를 사용하여 exGamepadState를 생성
+  for (const gamepad of gamepads) {
+    if (gamepad) {
+      // 게임패드의 모든 버튼 및 축을 순회하면서 상태 업데이트
+      gamepadState[gamepad.index] = {};
 
-            gamepad.buttons.forEach((button, index) => {
-                gamepadState[gamepad.index][`button${index + 1}`] = button.value;
-            });
+      gamepad.buttons.forEach((button, index) => {
+        gamepadState[gamepad.index][`button${index + 1}`] = button.value;
+      });
 
-            gamepad.axes.forEach((axis, index) => {
-                gamepadState[gamepad.index][`axis${index + 1}`] = axis;
-            });
-        }
+      gamepad.axes.forEach((axis, index) => {
+        gamepadState[gamepad.index][`axis${index + 1}`] = axis;
+      });
     }
-
-    // 콘솔에 게임패드 상태 출력
-    if (gamepadState != exGamepadState) {
-        console.log(gamepadState);
-        let now = + new Date();
-        console.log(now - exTime);
-        exTime = now;
-    }
-
-    // 다음 프레임 업데이트를 요청
-    setTimeout(updateGamepadState, pollingInterval);
+  }
+  let now = +new Date();
+  // 콘솔에 게임패드 상태 출력
+  if (JSON.stringify(gamepadState) !== JSON.stringify(exGamepadState)) {
+    // 객체 내부의 값을 비교
+    console.log(gamepadState);
+    let now = +new Date();
+    console.log(now - exTime);
+  }
+  exTime = now;
+  // 다음 프레임 업데이트를 요청
+  setTimeout(updateGamepadState, pollingInterval);
 }
 
-
 fullBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    if (myFace.requestFullscreen) {
-        myFace.requestFullscreen();
-    } else if (myFace.mozRequestFullScreen) {
-        myFace.mozRequestFullScreen();
-    } else if (myFace.webkitRequestFullscreen) {
-        myFace.webkitRequestFullscreen();
-    } else if (myFace.msRequestFullscreen) {
-        myFace.msRequestFullscreen();
-    }
+  event.preventDefault();
+  console.log("fullscreen");
+  if (screen.requestFullscreen) {
+    screen.requestFullscreen();
+  } else if (screen.mozRequestFullScreen) {
+    screen.mozRequestFullScreen();
+  } else if (screen.webkitRequestFullscreen) {
+    screen.webkitRequestFullscreen();
+  } else if (screen.msRequestFullscreen) {
+    screen.msRequestFullscreen();
+  }
+  var height = screen.offsetHeight;
+  const width = (height / 9) * 16;
+  video.style.width = width + "px";
 });
 
 // 최초 업데이트 시작
 updateGamepadState();
 
-
 //Soket join part
 
 async function initCall() {
-    makeConnection();
+  makeConnection();
 }
 
 async function handleWelcomeSubmit(event) {
-    await initCall();
-    socket.emit("join_room", roomName);
-
+  await initCall();
+  socket.emit("join_room", roomName);
 }
 
-
 handleWelcomeSubmit();
-
-
 
 //Socket part
 
 socket.on("welcome", async () => {
-    const offer = await myPeerConnection.createOffer();
-    myPeerConnection.setLocalDescription(offer);
-    console.log("sent the offer");
-    socket.emit("offer", offer, roomName);
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
 });
 
 socket.on("offer", async (offer) => {
-    console.log("received the offer");
-    myPeerConnection.setRemoteDescription(offer);
-    const answer = await myPeerConnection.createAnswer();
-    myPeerConnection.setLocalDescription(answer);
-    socket.emit("answer", answer, roomName);
-    console.log("sent the answer");
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
 });
 
 socket.on("answer", (answer) => {
-    console.log("received the answer");
-    myPeerConnection.setRemoteDescription(answer);
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
 });
 
 socket.on("ice", (ice) => {
-    console.log("received candidate");
-    myPeerConnection.addIceCandidate(ice);
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
 });
 
 // RTC part
 
 function makeConnection() {
-    myPeerConnection = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: [
-                    "stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302",
-                    "stun:stun2.l.google.com:19302",
-                    "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302",
-                ],
-            },
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
         ],
-    });
-    myPeerConnection.addEventListener("icecandidate", handleIce);
-    myPeerConnection.addEventListener("addstream", handleAddStream);
-    console.log("make connection");
+      },
+    ],
+  });
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  console.log("make connection");
 }
 
 function handleIce(data) {
-    console.log("sent candidate");
-    socket.emit("ice", data.candidate, roomName);
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
 }
 
 function handleAddStream(data) {
-    console.log("received the stream");
-    myFace.srcObject = data.stream;
+  console.log("received the stream");
+  video.srcObject = data.stream;
 }
--- src/public/js/server.js
+++ src/public/js/server.js
@@ -1,148 +1,116 @@
 const socket = io();
 
-const selectBtn = document.getElementById("selectdisplay");
 const myFace = document.getElementById("vod");
-const fullBtn = document.getElementById("full");
+const roomName = "upad";
+const selectBtn = document.getElementById("selectdisplay");
 
 let myStream;
 let myPeerConnection;
 
-const roomName = "upad";
-
-
-
 const displayMediaOptions = {
-    video: {
-        displaySurface: "browser",
-    },
-    audio: {
-        suppressLocalAudioPlayback: false,
-    },
-    preferCurrentTab: false,
-    selfBrowserSurface: "exclude",
-    systemAudio: "include",
-    surfaceSwitching: "include",
-    monitorTypeSurfaces: "include",
+  video: {
+    displaySurface: "browser",
+  },
+  audio: {
+    suppressLocalAudioPlayback: false,
+  },
+  preferCurrentTab: false,
+  selfBrowserSurface: "exclude",
+  systemAudio: "include",
+  surfaceSwitching: "include",
+  monitorTypeSurfaces: "include",
 };
 
-
 async function getMedia() {
-    console.log("get media");
-    try {
-        myStream = await navigator.mediaDevices.getDisplayMedia(
-            displayMediaOptions
-        );
-        console.log("get media success");
-        myFace.srcObject = myStream;
-        // console.log(myStream);
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    myStream = await navigator.mediaDevices.getDisplayMedia(
+      displayMediaOptions
+    );
+    myFace.srcObject = myStream;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function handledisplayChange(event) {
-    event.preventDefault();
-    await getMedia();
-    if (myPeerConnection) {
-        const videoTrack = myStream.getVideoTracks()[0];
-        const videoSender = myPeerConnection
-            .getSenders()
-            .find((sender) => sender.track.kind === "video");
-        videoSender.replaceTrack(videoTrack);
-        console.log("videoSender done");
-    }
+  event.preventDefault();
+
+  await getMedia();
+  if (myPeerConnection) {
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === "video");
+    videoSender.replaceTrack(videoTrack);
+  }
 }
 
-
-
+// camerasSelect.addEventListener("input", handleCameraChange);
 selectBtn.addEventListener("click", handledisplayChange);
-fullBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-
-    if (myFace.requestFullscreen) {
-        myFace.requestFullscreen();
-    } else if (myFace.mozRequestFullScreen) {
-        myFace.mozRequestFullScreen();
-    } else if (myFace.webkitRequestFullscreen) {
-        myFace.webkitRequestFullscreen();
-    } else if (myFace.msRequestFullscreen) {
-        myFace.msRequestFullscreen();
-    }
-});
-//connect part
-
+// Welcome Form (join a room)
 
 async function initCall() {
-    // welcome.hidden = true;
-    // call.hidden = false;
-    console.log("init call");
-    await getMedia();
-    makeConnection();
+  await getMedia();
+  makeConnection();
 }
-
 
 async function handleWelcomeSubmit() {
-    await initCall();
-    socket.emit("join_room", roomName);
+  await initCall();
+  socket.emit("join_room", roomName);
 }
-
-function makeConnection() {
-    myPeerConnection = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: [
-                    "stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302",
-                    "stun:stun2.l.google.com:19302",
-                    "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302",
-                ],
-            },
-        ],
-    });
-    myPeerConnection.addEventListener("icecandidate", handleIce);
-    myStream
-        .getTracks()
-        .forEach((track) => myPeerConnection.addTrack(track, myStream));
-    console.log("make connection");
-}
-
-
-function handleIce(data) {
-    console.log("sent candidate");
-    socket.emit("ice", data.candidate, roomName);
-}
-
-handleWelcomeSubmit();
-
+// Socket Code
 
 socket.on("welcome", async () => {
-    try {
-        const offer = await myPeerConnection.createOffer();
-        myPeerConnection.setLocalDescription(offer);
-        console.log("sent the offer");
-        socket.emit("offer", offer, roomName);
-    } catch (e) {
-        console.log(e)
-    }
-
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
 });
 
 socket.on("offer", async (offer) => {
-    console.log("received the offer");
-    myPeerConnection.setRemoteDescription(offer);
-    const answer = await myPeerConnection.createAnswer();
-    myPeerConnection.setLocalDescription(answer);
-    socket.emit("answer", answer, roomName);
-    console.log("sent the answer");
+  console.log("received the offer");
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
 });
 
 socket.on("answer", (answer) => {
-    console.log("received the answer");
-    myPeerConnection.setRemoteDescription(answer);
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
 });
 
 socket.on("ice", (ice) => {
-    console.log("received candidate");
-    myPeerConnection.addIceCandidate(ice);
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
 });
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);
+}
+
+handleWelcomeSubmit();
