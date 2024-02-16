247beaf897ea63b7b55b9dfd456b23bd881ec618
paul9748 <paul9748@hotmail.co.kr>
2024-02-16 10:52:49 +0900

    save

--- touch.cpp
+++ touch.cpp
@@ -1,119 +1,132 @@
 #include <node.h>
-#include <thread>
-#include <chrono>
-#include <vector>
-#include <nan.h>
 #include "parsec-vdd.h"
 
-using namespace v8;
-using namespace std::chrono_literals;
-using namespace parsec_vdd;
-
-// Global variables
-bool running = true;
-std::vector<int> displays;
-HANDLE vdd = NULL;
-
-// Function to add a virtual display
-void AddVirtualDisplay(const FunctionCallbackInfo<Value> &args)
+namespace ParsecAddon
 {
-    Isolate *isolate = args.GetIsolate();
 
-    // Check if the maximum number of displays has been reached
-    if (displays.size() >= VDD_MAX_DISPLAYS)
+    // Wrapper for DeviceStatus enum
+    enum DeviceStatus
     {
-        isolate->ThrowException(Exception::TypeError(
-            String::NewFromUtf8Literal(isolate, "Limit exceeded, could not add more virtual displays.")));
-        return;
+        DEVICE_OK = parsec_vdd::DEVICE_OK,
+        DEVICE_INACCESSIBLE = parsec_vdd::DEVICE_INACCESSIBLE,
+        DEVICE_UNKNOW = parsec_vdd::DEVICE_UNKNOW,
+        DEVICE_UNKNOW_PROBLEM = parsec_vdd::DEVICE_UNKNOW_PROBLEM,
+        DEVICE_DISABLED = parsec_vdd::DEVICE_DISABLED,
+        DEVICE_DRIVER_ERROR = parsec_vdd::DEVICE_DRIVER_ERROR,
+        DEVICE_RESTART_REQUIRED = parsec_vdd::DEVICE_RESTART_REQUIRED,
+        DEVICE_DISABLED_SERVICE = parsec_vdd::DEVICE_DISABLED_SERVICE,
+        DEVICE_NOT_INSTALLED = parsec_vdd::DEVICE_NOT_INSTALLED
+    };
+
+    // Wrapper for VddCtlCode enum
+    enum VddCtlCode
+    {
+        VDD_IOCTL_ADD = parsec_vdd::VDD_IOCTL_ADD,
+        VDD_IOCTL_REMOVE = parsec_vdd::VDD_IOCTL_REMOVE,
+        VDD_IOCTL_UPDATE = parsec_vdd::VDD_IOCTL_UPDATE,
+        VDD_IOCTL_VERSION = parsec_vdd::VDD_IOCTL_VERSION
+    };
+
+    // Wrapper for VddIoControl function
+    uint32_t VddIoControl(HANDLE vdd, VddCtlCode code, const void *data, size_t size)
+    {
+        return parsec_vdd::VddIoControl(vdd, static_cast<parsec_vdd::VddCtlCode>(code), data, size);
     }
 
-    // Add a new virtual display
-    int index = VddAddDisplay(vdd);
-    displays.push_back(index);
-
-    args.GetReturnValue().Set(index);
-}
-
-// Function to remove the last added virtual display
-void RemoveLastVirtualDisplay(const FunctionCallbackInfo<Value> &args)
-{
-    Isolate *isolate = args.GetIsolate();
-
-    // Check if there are any displays to remove
-    if (displays.empty())
+    // Wrapper for VddVersion function
+    int VddVersion(HANDLE vdd)
     {
-        isolate->ThrowException(Exception::TypeError(
-            String::NewFromUtf8Literal(isolate, "No added virtual displays.")));
-        return;
+        return parsec_vdd::VddVersion(vdd);
     }
 
-    // Remove the last virtual display
-    int index = displays.back();
-    VddRemoveDisplay(vdd, index);
-    displays.pop_back();
-
-    args.GetReturnValue().Set(index);
-}
-
-// Function to stop the program and clean up resources
-void StopProgram(const FunctionCallbackInfo<Value> &args)
-{
-    // Stop the main loop
-    running = false;
-
-    // Remove all virtual displays before exiting
-    for (int index : displays)
+    // Wrapper for VddUpdate function
+    void VddUpdate(HANDLE vdd)
     {
-        VddRemoveDisplay(vdd, index);
+        parsec_vdd::VddUpdate(vdd);
     }
 
-    // Close the device handle
-    if (vdd != NULL && vdd != INVALID_HANDLE_VALUE)
+    // Wrapper for VddAddDisplay function
+    int VddAddDisplay(HANDLE vdd)
     {
-        CloseDeviceHandle(vdd);
-        vdd = NULL;
-    }
-}
-
-// Function for the side thread to update vdd
-void UpdateVdd()
-{
-    while (running)
-    {
-        VddUpdate(vdd);
-        std::this_thread::sleep_for(100ms);
-    }
-}
-
-// Function to initialize the native addon
-void InitializeAddon(Local<Object> exports)
-{
-    // Check driver status
-
-    DeviceStatus status = QueryDeviceStatus(&VDD_CLASS_GUID, VDD_HARDWARE_ID);
-    if (status != DEVICE_OK)
-    {
-        Nan::ThrowError("Parsec VDD device is not OK.");
-        return;
+        return parsec_vdd::VddAddDisplay(vdd);
     }
 
-    // Obtain device handle
-    vdd = OpenDeviceHandle(&VDD_ADAPTER_GUID);
-    if (vdd == NULL || vdd == INVALID_HANDLE_VALUE)
+    // Wrapper for VddRemoveDisplay function
+    void VddRemoveDisplay(HANDLE vdd, int index)
     {
-        Nan::ThrowError("Failed to obtain the device handle.");
-        return;
+        parsec_vdd::VddRemoveDisplay(vdd, index);
     }
 
-    // Create a new thread for updating vdd
-    std::thread updater(UpdateVdd);
-    updater.detach();
+    // Wrapper for QueryDeviceStatus function
+    DeviceStatus QueryDeviceStatus(const char *deviceId)
+    {
+        return static_cast<DeviceStatus>(parsec_vdd::QueryDeviceStatus(&parsec_vdd::VDD_CLASS_GUID, deviceId));
+    }
 
-    // Define the functions to be exported
-    NODE_SET_METHOD(exports, "addVirtualDisplay", AddVirtualDisplay);
-    NODE_SET_METHOD(exports, "removeLastVirtualDisplay", RemoveLastVirtualDisplay);
-    NODE_SET_METHOD(exports, "stopProgram", StopProgram);
-}
+    // Wrapper for OpenDeviceHandle function
+    HANDLE OpenDeviceHandle()
+    {
+        return parsec_vdd::OpenDeviceHandle(&parsec_vdd::VDD_ADAPTER_GUID);
+    }
 
-// Register the addon initialization function
-NODE_MODULE(NativeAddon, InitializeAddon)
+    // Wrapper for CloseDeviceHandle function
+    void CloseDeviceHandle(HANDLE vdd)
+    {
+        parsec_vdd::CloseDeviceHandle(vdd);
+    }
+
+    // Wrapper for removing virtual displays up to numMonitors
+    void RemoveAllVirtualDisplays(HANDLE vdd, int numMonitors)
+    {
+        // Check if numMonitors is within valid range
+        if (numMonitors <= 0 || numMonitors > parsec_vdd::VDD_MAX_DISPLAYS)
+        {
+            // Handle the error as needed
+            printf("Invalid number of monitors specified.\n");
+            return;
+        }
+
+        // Logic to remove virtual displays up to numMonitors
+        std::vector<int> displaysToRemove;
+
+        // Get the current number of displays
+        int currentNumDisplays = VddIoControl(vdd, VDD_IOCTL_VERSION, nullptr, 0);
+
+        // Determine the number of displays to remove
+        int numDisplaysToRemove = std::min(numMonitors, currentNumDisplays);
+
+        // Collect the indices of displays to be removed
+        for (int i = 0; i < numDisplaysToRemove; ++i)
+        {
+            int index = VddIoControl(vdd, VDD_IOCTL_REMOVE, nullptr, 0);
+            displaysToRemove.push_back(index);
+        }
+
+        // Print the removed display indices
+        printf("Removed virtual displays up to index: ");
+        for (int index : displaysToRemove)
+        {
+            printf("%d, ", index);
+        }
+        printf("\n");
+
+        // Perform any additional cleanup or logging as needed
+    }
+
+    // Exported functions to Node.js
+    void Init(v8::Local<v8::Object> exports)
+    {
+        NODE_SET_METHOD(exports, "queryDeviceStatus", QueryDeviceStatus);
+        NODE_SET_METHOD(exports, "openDeviceHandle", OpenDeviceHandle);
+        NODE_SET_METHOD(exports, "closeDeviceHandle", CloseDeviceHandle);
+        NODE_SET_METHOD(exports, "vddIoControl", VddIoControl);
+        NODE_SET_METHOD(exports, "vddVersion", VddVersion);
+        NODE_SET_METHOD(exports, "vddUpdate", VddUpdate);
+        NODE_SET_METHOD(exports, "vddAddDisplay", VddAddDisplay);
+        NODE_SET_METHOD(exports, "vddRemoveDisplay", VddRemoveDisplay);
+        NODE_SET_METHOD(exports, "removeAllVirtualDisplays", RemoveAllVirtualDisplays);
+    }
+
+    NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
+
+} // namespace ParsecAddon
